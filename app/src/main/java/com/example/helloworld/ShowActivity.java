95a229112d10883d9990a902c3df814f51a8b2db
JonaBaron <133152403+JonaBaron@users.noreply.github.com>
2025-01-17 21:15:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/ShowActivity.java
@@ -0,0 +1,28 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ShowActivity extends AppCompatActivity {
+
+    public TextView tvShowName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_show);
+
+        String showName = getIntent().getStringExtra("name");
+        tvShowName = findViewById(R.id.tvShowName);
+        tvShowName.setText(showName);
+
+
+    }
+}
